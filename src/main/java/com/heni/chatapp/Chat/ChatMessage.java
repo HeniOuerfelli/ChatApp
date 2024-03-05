ce23a74a4fc12700a5ab9097cfb088e400fae080
heni-ouerfelli <132569078+Heni2002@users.noreply.github.com>
2024-03-05 12:50:48 +0100

    Initial commit

---
+++ src/main/java/com/heni/chatapp/Chat/ChatMessage.java
@@ -0,0 +1,14 @@
+package com.heni.chatapp.Chat;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ChatMessage {
+    private String content;
+    private String sender;
+    private MessageType type;
+}
---
+++ src/main/java/com/heni/chatapp/Chat/MessageType.java
@@ -0,0 +1,7 @@
+package com.heni.chatapp.Chat;
+
+public enum MessageType {
+    CHAT,
+    JOIN,
+    LEAVE
+}
